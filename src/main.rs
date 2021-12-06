24ef46042bea7f48a88fd0538d88f757f8d7830d
Cyril Plisko <cyril.plisko@mountall.com>
2021-12-06 21:27:50 +0200

    Move most of main body into Kopium::generate
    Signed-off-by: Cyril Plisko <cyril.plisko@mountall.com>

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ const KEYWORDS: [&str; 23] = [
 )]
 struct Kopium {
     #[structopt(about = "Give the name of the input CRD to use e.g. prometheusrules.monitoring.coreos.com")]
-    crd: String,
+    crd: Option<String>,
     #[structopt(about = "Use this CRD version if multiple versions are present", long)]
     api_version: Option<String>,
     #[structopt(about = "Do not emit prelude", long)]
@@ -39,80 +39,92 @@ struct Kopium {
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let kopium = Kopium::from_args();
-    let client = Client::try_default().await?;
-    let api: Api<CustomResourceDefinition> = Api::all(client);
-    let crd = api.get(&kopium.crd).await?;
-
-    let version = find_crd_version(&crd, kopium.api_version.as_deref())?;
-    let data = version
-        .schema
-        .as_ref()
-        .and_then(|schema| schema.open_api_v3_schema.clone());
-    let version = version.name.clone();
-
-    let kind = crd.spec.names.kind;
-    let plural = crd.spec.names.plural;
-    let group = crd.spec.group;
-    let scope = crd.spec.scope;
-
-    if let Some(schema) = data {
-        let mut structs = vec![];
-        log::debug!("schema: {}", serde_json::to_string_pretty(&schema)?);
-        analyze(schema, "", &kind, 0, &mut structs)?;
-
-        if !kopium.hide_prelude {
-            print_prelude(&structs);
-        }
-
-        for s in structs {
-            if s.level == 0 {
-                continue; // ignoring root struct
-            } else {
-                print_docstr(&kopium, s.docs, "");
-                if s.level == 1 && s.name.ends_with("Spec") {
-                    print_derives(&kopium.derive);
-                    println!(
-                        r#"#[kube(group = "{}", version = "{}", kind = "{}", plural = "{}")]"#,
-                        group, version, kind, plural
-                    );
-                    if scope == "Namespaced" {
-                        println!(r#"#[kube(namespaced)]"#);
-                    }
-                    // don't support grabbing original schema atm so disable schemas:
-                    // (we coerce IntToString to String anyway so it wont match anyway)
-                    println!(r#"#[kube(schema = "disabled")]"#);
-                    println!("pub struct {} {{", s.name);
-                } else {
-                    println!("#[derive(Serialize, Deserialize, Clone, Debug)]");
-                    let spec_trimmed_name = s.name.as_str().replace(&format!("{}Spec", kind), &kind);
-                    println!("pub struct {} {{", spec_trimmed_name);
-                }
-                for m in s.members {
-                    print_docstr(&kopium, m.docs, "    ");
-                    if let Some(annot) = m.field_annot {
-                        println!("    {}", annot);
-                    }
-                    let safe_name = if KEYWORDS.contains(&m.name.as_ref()) {
-                        format_ident!("r#{}", m.name)
-                    } else {
-                        format_ident!("{}", m.name)
-                    };
-                    let spec_trimmed_type = m.type_.as_str().replace(&format!("{}Spec", kind), &kind);
-                    println!("    pub {}: {},", safe_name, spec_trimmed_type);
-                }
-                println!("}}");
-                println!();
-            }
-        }
-    } else {
-        log::error!("no schema found for crd {}", kopium.crd);
-    }
-
-    Ok(())
+    Kopium::from_args().dispatch().await
 }
 
 impl Kopium {
+    async fn dispatch(&self) -> Result<()> {
+        let crds = Client::try_default()
+            .await
+            .map(Api::<CustomResourceDefinition>::all)?;
+        if let Some(name) = self.crd.as_deref() {
+            self.generate(crds, name).await
+        } else {
+            self.help()
+        }
+    }
+
+    async fn generate(&self, api: Api<CustomResourceDefinition>, name: &str) -> Result<()> {
+        let crd = api.get(name).await?;
+        let version = self.api_version.as_deref();
+        let version = find_crd_version(&crd, version)?;
+        let data = version
+            .schema
+            .as_ref()
+            .and_then(|schema| schema.open_api_v3_schema.clone());
+        let version = version.name.clone();
+
+        let kind = crd.spec.names.kind;
+        let plural = crd.spec.names.plural;
+        let group = crd.spec.group;
+        let scope = crd.spec.scope;
+
+        if let Some(schema) = data {
+            let mut structs = vec![];
+            log::debug!("schema: {}", serde_json::to_string_pretty(&schema)?);
+            analyze(schema, "", &kind, 0, &mut structs)?;
+
+            if !self.hide_prelude {
+                print_prelude(&structs);
+            }
+
+            for s in structs {
+                if s.level == 0 {
+                    continue; // ignoring root struct
+                } else {
+                    print_docstr(self, s.docs, "");
+                    if s.level == 1 && s.name.ends_with("Spec") {
+                        print_derives(&self.derive);
+                        println!(
+                            r#"#[kube(group = "{}", version = "{}", kind = "{}", plural = "{}")]"#,
+                            group, version, kind, plural
+                        );
+                        if scope == "Namespaced" {
+                            println!(r#"#[kube(namespaced)]"#);
+                        }
+                        // don't support grabbing original schema atm so disable schemas:
+                        // (we coerce IntToString to String anyway so it wont match anyway)
+                        println!(r#"#[kube(schema = "disabled")]"#);
+                        println!("pub struct {} {{", s.name);
+                    } else {
+                        println!("#[derive(Serialize, Deserialize, Clone, Debug)]");
+                        let spec_trimmed_name = s.name.as_str().replace(&format!("{}Spec", kind), &kind);
+                        println!("pub struct {} {{", spec_trimmed_name);
+                    }
+                    for m in s.members {
+                        print_docstr(self, m.docs, "    ");
+                        if let Some(annot) = m.field_annot {
+                            println!("    {}", annot);
+                        }
+                        let safe_name = if KEYWORDS.contains(&m.name.as_ref()) {
+                            format_ident!("r#{}", m.name)
+                        } else {
+                            format_ident!("{}", m.name)
+                        };
+                        let spec_trimmed_type = m.type_.as_str().replace(&format!("{}Spec", kind), &kind);
+                        println!("    pub {}: {},", safe_name, spec_trimmed_type);
+                    }
+                    println!("}}");
+                    println!();
+                }
+            }
+        } else {
+            log::error!("no schema found for crd {}", name);
+        }
+
+        Ok(())
+    }
+
     fn help(&self) -> Result<()> {
         Self::clap().print_help().map(|_| println!())?;
         Ok(())
