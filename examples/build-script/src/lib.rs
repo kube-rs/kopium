abd994fa92d10d317618a7f9d6acea3c6da666c2
clux <sszynrae@gmail.com>
2025-10-10 16:55:19 +0100

    lift crate specifics into an more realistic example
    Signed-off-by: clux <sszynrae@gmail.com>

---
+++ examples/build-script/src/lib.rs
@@ -0,0 +1,18 @@
+//! This library is exporting generated types from the `kopium` build script.
+//!
+//! For more information, see the [kopium examples](https://github.com/kube-rs/kopium/blob/main/examples/)
+
+mod generated;
+
+#[allow(unused_imports)]
+pub use generated::*;
+
+// Note: anything added to or changed in the `generated` modules (i.e. `generated.rs`, *or* `generated/*.rs`)
+// will be overwritten by the build script, so any extension methods/impls for the generated types should be added here
+
+impl Alertmanager {
+    /// A custom method for one of the generated types that won't be overwritten by the build script
+    pub fn some_custom_method(&self) -> bool {
+        todo!()
+    }
+}
